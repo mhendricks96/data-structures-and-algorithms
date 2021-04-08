7be71d7d1d6047eeb63207d21ee0e1fa41dffada
mhendricks96 <mhendricks96@yahoo.com>
2021-04-08 16:14:20 -0700

    finished code challenge 6

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -81,6 +81,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 
 };
 
