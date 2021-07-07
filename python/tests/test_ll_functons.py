8d7581274c73c9b86a9155b183e8584f0f9175a8
mhendricks96 <mhendricks96@yahoo.com>
2021-07-06 21:14:28 -0700

    giving up on old stretch goal

--- python/tests/test_ll_functons.py
+++ python/tests/test_ll_functons.py
@@ -1,4 +1,4 @@
-from ll_functions.ll_functions import zipped_list, merge_sorted_list
+from ll_functions.ll_functions import zipped_list#, merge_sorted_list
 from linked_list.linked_list import LinkedList, Node
 
 def test_zipped_list():
@@ -9,12 +9,12 @@ def test_zipped_list():
     expected = "{'a'} -> {'1'} -> {'b'} -> {'2'} -> {'c'} -> {'3'} -> NULL"
     assert actual == expected
 
-def test_merge_sorted_list():
-    ll1 = LinkedList()
-    ll1.insert(1)
-    ll1.insert(2)
-    ll1.insert(3)
-    ll2 = LinkedList(Node(2, Node(4, Node(6))))
-    actual = merge_sorted_list(ll1,ll2)
-    expected = "{1} -> {2} -> {3} -> {4} -> {5} -> {6} -> NULL"
-    assert actual == expected
+# def test_merge_sorted_list():
+#     ll1 = LinkedList()
+#     ll1.insert(1)
+#     ll1.insert(2)
+#     ll1.insert(3)
+#     ll2 = LinkedList(Node(2, Node(4, Node(6))))
+#     actual = merge_sorted_list(ll1,ll2)
+#     expected = "{1} -> {2} -> {3} -> {4} -> {5} -> {6} -> NULL"
+#     assert actual == expected
