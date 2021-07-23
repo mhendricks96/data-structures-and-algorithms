36fab402d6c6510ae03402847f40b204ad5ff348
mhendricks96 <mhendricks96@yahoo.com>
2021-07-23 13:48:36 -0700

    finished contains function

--- python/binary_tree/binary_tree.py
+++ python/binary_tree/binary_tree.py
@@ -7,8 +7,8 @@ class Node:
 
 class BinaryTree:
   
-  def __init__(self, root=None):
-    self.root = root
+  # def __init__(self, root=None):
+  #   self.root = root
 
   
   
@@ -28,45 +28,62 @@ class BinaryTree:
 class BinarySearchTree(BinaryTree):
   
   def __init__(self, root=None,):
-    super().__init__(root=None,)
+    # super().__init__(root=None,)
+    self.root = root
+    self.left_child = None
+    self.right_child = None
 
-  def add(self, value=None,):
+  def add(self, value):
     """
     Adds a new node with that value in the correct location in the binary search tree.
     """
-    #new_node = Node(value)
-    #current = self.root
     # check if tree is empty
     if self.root is None:
       #if so, new node becomes the root
-      self.root = Node(value)
+      self.root = value
       return
 
-    elif self.root.value == value:
+    if self.root == value:
       # not sure what to do with duplicates yet
+      #can change below comparison to "less than or equal to, or use a counter"
       pass
       return
 
-    
-    elif self.root.value < value:
-      #go to right subtree
-      #run add() recursevly on right subtree
-      if self.root.right_child:
-        self.right_child.add(value)
       
-
-    else:
+    if self.root > value:
       #got to left subtree
       #check if left subtree is empty
       #run add() recursively on the left subtree
-      if self.root.left_child:
+      if self.left_child:
         self.left_child.add(value)
+      else:
+        self.left_child = BinarySearchTree(value)
+
+    else:
+      #go to right subtree
+      #run add() recursevly on right subtree
+      if self.right_child:
+        self.right_child.add(value)
+      else:
+        self.right_child = BinarySearchTree(value)
 
 
-
-  def contains(self, value=None):
+  def contains(self, value):
     """
     Returns: boolean indicating whether or not the value is in the tree at least once.
 
     """
-    pass
+    if self.root == value:
+      return True
+
+    if self.root > value:
+      if self.left_child:
+        return self.left_child.contains(value)
+      else:  
+        return False
+      
+    else:
+      if self.right_child:
+        return self.right_child.contains(value)
+      else:
+        return False
--- python/tests/test_binary_tree.py
+++ python/tests/test_binary_tree.py
@@ -11,7 +11,7 @@ def test_empty_search():
 def test_search_tree_with_root():
   my_search = BinarySearchTree()
   my_search.add(15)
-  actual = my_search.root.value
+  actual = my_search.root
   expected = 15
   assert actual == expected
 
@@ -20,19 +20,62 @@ def test_adding_nodes_to_root():
   my_search.add(15)
   my_search.add(1)
   my_search.add(32)
-  actual = my_search.root.value
+  actual = my_search.root
   expected = 15
   assert actual == expected
 
 def test_adding_many_nodes():
   my_search = BinarySearchTree()
   my_search.add(19)
-  my_search.add(1)
-  my_search.add(32)
-  my_search.add(17)
-  my_search.add(6)
-  my_search.add(18)
-  actual = my_search.root.value
+  values_list = [17,1,32,16,6,18]
+  for number in values_list:
+    my_search.add(number)
+  actual = my_search.root
   expected = 19
   assert actual == expected
 
+def test_contains_found_1():
+  my_search = BinarySearchTree()
+  my_search.add(50)
+  values_list = [17,10,32,16,6,18]
+  for number in values_list:
+    my_search.add(number)
+
+  assert my_search.contains(32)
+
+def test_contains_found_all():
+  my_search = BinarySearchTree()
+  my_search.add(50)
+  values_list = [17,10,32,16,6]
+  for number in values_list:
+    my_search.add(number)
+
+  assert my_search.contains(50)
+  assert my_search.contains(17)
+  assert my_search.contains(10)
+  assert my_search.contains(32)
+  assert my_search.contains(16)
+  assert my_search.contains(6)
+
+def test_contain_wrong_1():
+  my_search = BinarySearchTree()
+  my_search.add(50)
+  values_list = [17,10,32,16,6,18]
+  for number in values_list:
+    my_search.add(number)
+
+  assert not my_search.contains(99)
+
+def test_contain_many_wrong():
+  my_search = BinarySearchTree()
+  my_search.add(50)
+  values_list = [17,10,32,16,6]
+  for number in values_list:
+    my_search.add(number)
+
+  assert not my_search.contains(7)
+  assert not my_search.contains(15)
+  assert not my_search.contains(33)
+  assert not my_search.contains(77)
+  assert not my_search.contains(86)
+  assert not my_search.contains(99)
