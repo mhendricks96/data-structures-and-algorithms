cedc20a977bf6dd52c9f48487214135f11eca6da
mhendricks96 <mhendricks96@yahoo.com>
2021-08-02 23:56:14 -0700

    working

--- python/k_ary_tree/k_ary_tree.py
+++ python/k_ary_tree/k_ary_tree.py
@@ -1,9 +1,48 @@
-from ll_queue.ll_queue import LL_Queue
+#from ll_queue.ll_queue import LL_Queue
 
-# class Node:
-#   def __init__(self, value=None, children=[]):
-#     self.value = value
-#     self.children = children
+class Node:
+  def __init__(self, value=None, next=None, children=[]):
+    self.value = value
+    self.children = children
+    self.next = next
+
+class Queue:
+    def __init__(self, front=None, rear=None):
+        self.front = front
+        self.rear = rear
+        self.queue_size = 0
+
+    def __len__(self):
+        """keeps tack of length of queue"""
+        return self.queue_size
+
+    def is_empty(self):
+        """checks to see if queue is empty""" 
+        return self.queue_size == 0
+
+
+    def enqueue(self, value):
+        """adds new node to rear of queue"""
+        new_node = Node(value)
+        if self.is_empty():
+            self.front = new_node
+        else:
+            self.rear.next = new_node
+        self.rear = new_node
+        self.queue_size += 1
+
+    def dequeue(self):
+        """
+        removes node from front of queue and returns the result
+        """
+        if self.is_empty():
+            return ("Sorry, the queue is empty")
+        result = self.front.value
+        self.front = self.front.next
+        self.queue_size -= 1
+        return result
+
+
 
 class KaryTree:
   def __init__(self, root=None, children=[]):
@@ -13,15 +52,18 @@ class KaryTree:
  
   def level_order(self):
     values_list = [self.root]
-    queue = LL_Queue()
+    queue = Queue()
 
     queue.enqueue(self)
-    while queue:
-      front=queue.dequeue()
-      if front==None:
+    while not queue.is_empty():
+
+      front = queue.front
+      if front == None:
             continue
       for child in front.children:
-        values_list.append(child.root)
+        queue.enqueue(child.root)
+      values_list.append(front.value.root)
+      queue.dequeue()
 
     return values_list
 
--- python/tests/test_k_ary_tree.py
+++ python/tests/test_k_ary_tree.py
@@ -1,4 +1,4 @@
-from k_ary_tree.k_ary_tree import KaryTree
+from k_ary_tree.k_ary_tree import KaryTree, Node, Queue
 import pytest
 
 
@@ -7,46 +7,20 @@ def test_tree_with_single_node():
   #my_tree.children.append(KaryTree(9))
   assert my_tree.count == 1
 
-def test_level_order_traversal():
-  my_tree = KaryTree(8)
-  my_tree.children.append(KaryTree(9))
-  my_tree.children.append(KaryTree(10))
-  actual = my_tree.level_order()
-  expected = [8,9,10]
+
+# def test_level_order_traversal(my_tree):
+  
+#   actual = my_tree.level_order()
+#   expected = [8,12,10,7,15]
+#   assert actual == expected
+
+def test_level_order_traversal_2(my_big_tree):
+  
+  actual = my_big_tree.level_order()
+  expected = [8,12,10,7,15,99,50]
   assert actual == expected
 
-# def test_pre_order_1_node():
-#   my_tree = KaryTree()
-#   my_tree.insert(8)
-#   actual = my_tree.pre_order_traversal()
-#   expected = [8]
-#   assert actual == expected
 
-# def test_adding_many_nodes():
-#   my_k_tree = KaryTree()
-#   my_k_tree.insert(15)
-#   my_k_tree.insert(88,15)
-#   my_k_tree.insert(123,15)
-#   my_k_tree.insert(2,88)
-#   assert my_k_tree.count == 4
-
-# def test_pre_order_traversal():
-#   my_k_tree = KaryTree()
-#   my_k_tree.insert(15)
-#   my_k_tree.insert(88,15)
-#   my_k_tree.insert(123,15)
-#   my_k_tree.insert(2,88)
-#   actual = my_k_tree.pre_order_traversal()
-#   expected = [15, 88, 2, 123]
-#   assert actual == expected
-
-
-# def test_adding_fixture(my_tree):
-#   assert my_tree.count == 6
-
-# def test_pre_order_traversal(my_tree):
-#   actual = my_tree.pre_order_traversal
-#   expected = []
 
 
 
@@ -57,16 +31,25 @@ def test_level_order_traversal():
 
 @pytest.fixture
 def my_tree():
-  my_tree = KaryTree()
-  my_tree.insert(17)
-  my_tree.insert(99,17)
-  my_tree.insert(123,99)
-  # my_tree.insert(2,99)
-  # my_tree.insert(15,17)
-  # my_tree.insert(88,17)
+  my_tree = KaryTree(8)
+  my_tree.children.append(KaryTree(12))
+  my_tree.children.append(KaryTree(10))
+  my_tree.children.append(KaryTree(7))
+  my_tree.children.append(KaryTree(15))
   return my_tree
 
+@pytest.fixture
+def my_big_tree():
+  my_big_tree = KaryTree(8)
+  my_big_tree.children.append(KaryTree(12))
+  my_big_tree.children.append(KaryTree(10))
+  my_big_tree.children.append(KaryTree(7))
+  my_big_tree.children.append(KaryTree(15))
+  my_big_tree.children[0].children.append(KaryTree(99))
+  my_big_tree.children[2].children.append(KaryTree(50))
+  return my_big_tree
 
   @pytest.fixture(autouse=True)
   def clean():
     my_tree = None
+    my_big_tree = None
