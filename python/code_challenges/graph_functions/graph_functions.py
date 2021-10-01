33c8a4569ebecabe2124d978737b139e732dd402
mhendricks96 <mhendricks96@yahoo.com>
2021-09-30 21:29:54 -0700

    finished cc 37

--- python/code_challenges/graph_functions/graph_functions.py
+++ python/code_challenges/graph_functions/graph_functions.py
@@ -3,10 +3,27 @@ from graphs.graphs import Graph
 
 def business_trip(my_graph, list_of_cities):
   '''function that takes in a list of cities and returns a boolean indicating whether the trip is possible with direct flights'''
+  
+  if my_graph.get_nodes() == None or list_of_cities == None:
+    return False, 0
+  
   total_price = 0
-  happy_path = False
-  #city_list = []
+  
+  for idx, city in enumerate(list_of_cities):
+    possible_flights = my_graph.get_neighbors(city)
 
-  for city in (list_of_cities):
-    city_list = my_graph._adjacency_list[city]
-  return city_list
+    if idx + 1 < len(list_of_cities):
+      next_city = list_of_cities[idx + 1]
+    else:
+      next_city = None
+
+    if next_city == None:
+      break
+    elif next_city not in possible_flights:
+      return False, 0
+
+    ticket_price = possible_flights[next_city]
+
+    total_price = total_price + ticket_price
+  
+  return True, total_price
