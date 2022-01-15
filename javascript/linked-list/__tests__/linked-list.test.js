838921ecfa6c577d221c58c0f989a6cff76ca53b
michael <mhendricks96@yahoo.com>
2022-01-14 16:48:36 -0800

    done with implementation, next up is testing

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,14 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
 
 describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
+
+  
 });
+
+
