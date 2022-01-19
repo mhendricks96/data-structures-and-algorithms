3c1c4d9844a965c0c65a7bebcff3cd9db0147a9d
michael <mhendricks96@yahoo.com>
2022-01-18 21:38:53 -0800

    refactored code for CC%/implementation

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { it } = require('eslint/lib/rule-tester/rule-tester');
+const { it, describe } = require('eslint/lib/rule-tester/rule-tester');
 // Require our linked list implementation
 const LinkedList = require('../index');
 
@@ -11,21 +11,55 @@ describe('Linked List', () => {
 });
 
 describe('Testing Linked List creation', () => {
-  it('should return that the array i created', () => {
+  it('should instantiate an empty linked list', () => {
     let my_list = new LinkedList();
-    my_list.append(5); // append method works
+    expect(my_list.length).toEqual(0);
+  });
+});
+
+describe('Testing the Head Node', () => {
+  it('should point to the first node in the linked list', () => {
+    let my_list = new LinkedList();
+    my_list.append(5);
+    expect(my_list.head.value).toEqual(5);
+  });
+});
+
+describe('Testing Insert function', () => {
+  it('should insert multiple nodes', () => {
+    let my_list = new LinkedList();
+    my_list.insert(15);
+    my_list.insert(78);
+    my_list.insert(456);
+    expect(my_list.to_string()).toEqual('{ 456 } -> { 78 } -> { 15 } -> NULL');
+  });
+});
+
+describe('Testing includes function', () => {
+  it('should return a value of true when finding the value', () => {
+    let my_list = new LinkedList();
+    my_list.append(5);
     my_list.append(3);
     my_list.append(7);
-    expect(my_list.length).toEqual(3);
-    expect(my_list.head.value).toEqual(5);
-    expect(my_list.includes(48)).toEqual(false);
     expect(my_list.includes(5)).toEqual(true);
+  });
+  it('should return a value of false when not finding the value', () => {
+    let my_list = new LinkedList();
+    my_list.append(5);
+    my_list.append(3);
+    my_list.append(7);
+    expect(my_list.includes(48)).toEqual(false);
+  });
+});
+
+
+describe('Testing string function', () => {
+  it('should return a string containing all of the nodes', () => {
+    let my_list = new LinkedList();
+    my_list.append(5);
+    my_list.append(3);
+    my_list.append(7);
     expect(my_list.to_string()).toEqual('{ 5 } -> { 3 } -> { 7 } -> NULL');
-    my_list.insert(15);
-    expect(my_list.head.value).toEqual(15);
-    let my_list2 = new LinkedList();
-    my_list2.insert(99);
-    expect(my_list2.head.value).toEqual(99);
   });
 });
 
